deda33875916766a8afac99d66697ab44e670c9c
Yaopeng Wu <yaopeng.wu@chartboost.com>
2017-12-31 16:19:13 -0800

    formatting

--- chapter-futures/src/main/scala/com/goticks/TicketInfo.scala
+++ chapter-futures/src/main/scala/com/goticks/TicketInfo.scala
@@ -2,18 +2,19 @@ package com.goticks
 
 import org.joda.time.{Duration, DateTime}
 
+//Key: optional values should come after mandatory values
 case class TicketInfo(ticketNr: String,
-                      event: Option[Event] = None,
                       userLocation: Location,
+                      event: Option[Event] = None,
                       travelAdvice: Option[TravelAdvice] = None,
                       weather: Option[Weather] = None,
                       suggestions: Seq[Event] = Seq())
 
+case class Location(lat: Double, lon: Double)
+
 case class Event(name: String, location: Location, time: DateTime)
 
 case class Artist(name: String, calendarUri: String)
-
-case class Location(lat: Double, lon: Double)
 
 case class TravelAdvice(routeByCar: Option[RouteByCar] = None,
                         publicTransportAdvice: Option[PublicTransportAdvice] = None)
